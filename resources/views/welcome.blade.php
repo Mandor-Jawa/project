b0c6bd993b762ecdefbbedcd60ac790ba0af6435
Fikri Akbar <akbarfikri819@gmail.com>
2026-07-20 16:11:50 +0700

    update 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Proposal Tracker</title>
+    <link rel="icon" href="{{ asset('assets/images/Icon.png') }}" type="image/png">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
